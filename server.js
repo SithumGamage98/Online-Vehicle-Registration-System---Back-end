91f8aa863cabd001f78fd765ae60c350228f010a
Sankalpa69 <100986253+Sankalpa69@users.noreply.github.com>
2022-08-07 08:20:37 +0530

    Initial backend commit

---
+++ server.js
@@ -0,0 +1,45 @@
+const express=require("express");
+const mongoose=require("mongoose");
+const bodyParser=require("body-parser");
+const cors=require("cors");
+const app=express();
+require("dotenv").config();
+
+
+const PORT = process.env.PORT || 8070;
+
+//Import Routesss
+/*const PackageRoutes=require('./routes/packages');
+const FeedBackRoutes=require('./routes/feedbacks'); */
+
+app.use(cors());
+app.use(bodyParser.json());
+//app.use(PackageRoutes);
+//app.use(FeedBackRoutes);
+
+const URL=process.env.MONGODB_URL;
+
+mongoose.connect(URL, {
+
+    //useCreateIndex : true,
+    useNewUrlParser : true,
+    //useUnifiedTopologyL:true,
+    //useFindAndModify : false
+
+}); 
+
+
+const connection = mongoose.connection;
+connection.once('open',() => {
+ console.log("Mongodb connection success!");
+
+}) 
+
+
+
+
+app.listen(PORT, () => {
+
+    console.log(`Server is up and running on port number: ${PORT}`)
+
+}) 
